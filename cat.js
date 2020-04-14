6052c4c212b338cc6713be5fd2a9be666d3beef7
HL <hoanglinh171038@gmail.com>
2020-04-14 14:43:59 +0700

    improve cat eat

--- cat.js
+++ cat.js
@@ -1,5 +1,17 @@
+var Mouse = require('./mouse');
 function cat()
 {
 	this.stomatch = [];
 }
+cat.prototype.eat = function(animal) {
+	if(animal instanceof Mouse)
+	{
+		this.stomatch.push(animal);
+	}
+	else
+	{
+		throw new Error('Cat can only eat Mouse');
+	}
+	
+};
 module.exports = cat;
